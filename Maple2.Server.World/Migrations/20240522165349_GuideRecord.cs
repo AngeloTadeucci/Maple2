e945f9efea80b7653927b260d5512612f5ef6592
Zin <62830952+Zintixx@users.noreply.github.com>
2024-05-22 09:54:39 -0700

    Guide Records

---
+++ Maple2.Server.World/Migrations/20240522165349_GuideRecord.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Maple2.Server.World.Migrations
+{
+    /// <inheritdoc />
+    public partial class GuideRecord : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DeathPenalty",
+                table: "character-config",
+                newName: "GuideRecords");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DeathCount",
+                table: "character-config",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<long>(
+                name: "DeathTick",
+                table: "character-config",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeathCount",
+                table: "character-config");
+
+            migrationBuilder.DropColumn(
+                name: "DeathTick",
+                table: "character-config");
+
+            migrationBuilder.RenameColumn(
+                name: "GuideRecords",
+                table: "character-config",
+                newName: "DeathPenalty");
+        }
+    }
+}
