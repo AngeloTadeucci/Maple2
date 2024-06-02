1b16fc743fd32462307f45df218bbbecf2996406
budomike <89367969+budomike@users.noreply.github.com>
2024-06-02 14:03:40 -0700

    Update Maple2.Database.Seed/Program.cs
    Co-authored-by: Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>

--- Maple2.Database.Seed/Program.cs
+++ Maple2.Database.Seed/Program.cs
@@ -99,7 +99,7 @@ bool IsTableEmpty(string tableName) {
         if (entityType != null) {
             var method = typeof(DbContext).GetMethod(nameof(DbContext.Set), new Type[] { });
             var genericMethod = method?.MakeGenericMethod(entityType);
-            var queryable = (IQueryable<object>) genericMethod?.Invoke(ms2Context, null);
+            var queryable = (IQueryable<object>?) genericMethod?.Invoke(ms2Context, null);
 
             bool isEmpty = !queryable?.Any() ?? true;
             Console.WriteLine($"Table {tableName} is empty.");
