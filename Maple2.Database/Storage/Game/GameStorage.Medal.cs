1952738c7ca6aa54665dbac8f550d781ab72379e
Zin <62830952+Zintixx@users.noreply.github.com>
2024-06-10 01:24:12 -0700

    Survival Medals

---
+++ Maple2.Database/Storage/Game/GameStorage.Medal.cs
@@ -0,0 +1,51 @@
+﻿using Maple2.Database.Extensions;
+using Maple2.Model.Enum;
+using Maple2.Model.Game;
+using Maple2.Tools.Extensions;
+
+namespace Maple2.Database.Storage;
+
+public partial class GameStorage {
+    public partial class Request {
+        public List<Medal> GetMedals(long ownerId) {
+            return Context.Medal.Where(medal => medal.OwnerId == ownerId)
+                .AsEnumerable()
+                .Select(ToMedal)
+                .WhereNotNull()
+                .ToList();
+        }
+
+        public Medal? CreateMedal(long ownerId, Medal medal) {
+            Model.Medal model = medal;
+            model.OwnerId = ownerId;
+            model.Id = 0;
+            Context.Medal.Add(model);
+
+            return Context.TrySaveChanges() ? ToMedal(model) : null;
+        }
+
+        public bool SaveMedals(long ownerId, params Medal[] medals) {
+            var models = new Model.Medal[medals.Length];
+            for (int i = 0; i < medals.Length; i++) {
+                if (medals[i].Uid == 0) {
+                    continue;
+                }
+
+                models[i] = medals[i];
+                models[i].OwnerId = ownerId;
+                Context.Medal.Update(models[i]);
+            }
+
+            return Context.TrySaveChanges();
+        }
+
+        // Converts model to item if possible, otherwise returns null.
+        private Medal? ToMedal(Model.Medal? model) {
+            if (model == null) {
+                return null;
+            }
+
+            return game.tableMetadata.SurvivalSkinInfoTable.Entries.TryGetValue(model.MedalId, out MedalType medalType) ? model.Convert(medalType) : null;
+        }
+    }
+}
---
+++ Maple2.Server.World/Migrations/20240610051805_Medal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Maple2.Server.World.Migrations
+{
+    /// <inheritdoc />
+    public partial class Medal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "medal",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    OwnerId = table.Column<long>(type: "bigint", nullable: false),
+                    MedalId = table.Column<int>(type: "int", nullable: false),
+                    Slot = table.Column<short>(type: "smallint", nullable: false),
+                    ExpiryTime = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_medal", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "medal");
+        }
+    }
+}
