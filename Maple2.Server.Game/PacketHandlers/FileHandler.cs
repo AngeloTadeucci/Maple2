af6004d4d927f6093f152aad40f6a8ea1827fd40
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2024-07-12 01:22:40 -0300

    Update FileHandler.cs

--- Maple2.Server.Game/PacketHandlers/FileHandler.cs
+++ Maple2.Server.Game/PacketHandlers/FileHandler.cs
@@ -2,17 +2,17 @@ using Maple2.PacketLib.Tools;
 using Maple2.Server.Core.Constants;
 using Maple2.Server.Core.PacketHandlers;
 using Maple2.Server.Game.Session;
+using Serilog;
 
 namespace Maple2.Server.Game.PacketHandlers;
 public class FileHashHandler : PacketHandler<GameSession> {
     public override RecvOp OpCode => RecvOp.FileHash;
 
     public override void Handle(GameSession session, IByteReader packet) {
-        System.Console.WriteLine(packet);
         packet.ReadInt();
         string filename = packet.ReadString();
         string md5 = packet.ReadString();
 
-        System.Console.WriteLine($"Hash for {filename}: {md5}");
+        Log.Logger.Debug($"Hash for {filename}: {md5}");
     }
 }
