284a3c55ae3227445c0c55d226df6c9bbe643d83
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2024-09-04 22:16:45 -0300

    Fix: FieldEnterPacket

--- Maple2.Server.Game/Manager/Field/FieldManager.cs
+++ Maple2.Server.Game/Manager/Field/FieldManager.cs
@@ -67,7 +67,7 @@ public sealed partial class FieldManager : IDisposable {
     public int MapId => Metadata.Id;
     public readonly long OwnerId;
     public readonly int InstanceId;
-    public FieldInstance FieldInstance = default;
+    public FieldInstance FieldInstance = new();
     public readonly AiManager Ai;
     public IFieldRenderer? DebugRenderer { get; private set; }
 
