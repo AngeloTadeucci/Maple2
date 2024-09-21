54897dbb266766c103d4ee7270ab1c8af10412d4
sleepy-day <matthew@sleepy.day>
2024-09-21 13:37:20 +1000

    undo one of the using changes incorrectly added

--- Maple2.Server.Core/Modules/DataDbModule.cs
+++ Maple2.Server.Core/Modules/DataDbModule.cs
@@ -30,7 +30,7 @@ public class DataDbModule : Module {
 
     protected override void Load(ContainerBuilder builder) {
         // NoTracking, Metadata is cached separately, and we use a single context for lifetime.
-        using var context = new MetadataContext(options);
+        var context = new MetadataContext(options);
         context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
         builder.RegisterInstance(context);
 
