54fbe5d5cac5d63a5c7817f731f063a81f1958e2
Edmundo Marvão <emundomarvao@gmail.com>
2024-10-02 14:32:49 +0100

    Remove EncryptionHelper

--- Maple2.Server.Core/Helpers/EncryptionHelper.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Maple2.Server.Core.Helpers;
-
-public class EncryptionHelper {
-    public static string HashPassword(string password) => BCrypt.Net.BCrypt.HashPassword(password, 13);
-
-    public static bool VerifyPassword(string enteredPassword, string storedHash) => BCrypt.Net.BCrypt.Verify(enteredPassword, storedHash);
-}
