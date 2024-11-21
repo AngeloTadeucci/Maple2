35f69e4e1e7dceb6f1e29043d5a77cbd7c6924a5
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2024-11-20 22:19:50 -0300

    Remove hardcoded database name from migrations (#292)

--- Maple2.Server.World/Migrations/20221114194917_RenameStatOption.cs
+++ Maple2.Server.World/Migrations/20221114194917_RenameStatOption.cs
@@ -5,20 +5,20 @@
 namespace Maple2.Server.World.Migrations {
     public partial class RenameStatOption : Migration {
         protected override void Up(MigrationBuilder migrationBuilder) {
-            migrationBuilder.Sql(@"UPDATE `game-server`.item SET
+            migrationBuilder.Sql(@"UPDATE `item` SET
                 Stats = REPLACE(Stats, '""StatOption"":', '""BasicOption"":')");
-            migrationBuilder.Sql(@"UPDATE `game-server`.item SET
+            migrationBuilder.Sql(@"UPDATE `item` SET
                 Enchant = REPLACE(Enchant, '""StatOption"":', '""BasicOption"":')");
-            migrationBuilder.Sql(@"UPDATE `game-server`.item SET
+            migrationBuilder.Sql(@"UPDATE `item` SET
                 LimitBreak = REPLACE(LimitBreak, '""StatOption"":', '""BasicOption"":')");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder) {
-            migrationBuilder.Sql(@"UPDATE `game-server`.item SET
+            migrationBuilder.Sql(@"UPDATE `item` SET
                 Stats = REPLACE(Stats, '""BasicOption"":', '""StatOption"":')");
-            migrationBuilder.Sql(@"UPDATE `game-server`.item SET
+            migrationBuilder.Sql(@"UPDATE `item` SET
                 Enchant = REPLACE(Enchant, '""BasicOption"":', '""StatOption"":')");
-            migrationBuilder.Sql(@"UPDATE `game-server`.item SET
+            migrationBuilder.Sql(@"UPDATE `item` SET
                 LimitBreak = REPLACE(LimitBreak, '""BasicOption"":', '""StatOption"":')");
         }
     }
