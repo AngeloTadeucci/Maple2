a806570438ab758ce062e9e44385c50efc13258d
Tom <167194956+Dreary@users.noreply.github.com>
2025-02-20 21:27:51 +0000

    Transparency Badge, Meso + Meret limit changes (+dotnet format) (#316)
    * Transparency Badge, Meso + Meret limit changes (+ dotnet format)
    
    * Update Maple2.Server.Game/PacketHandlers/BadgeEquipHandler.cs
    
    Co-authored-by: Zin <62830952+Zintixx@users.noreply.github.com>
    
    * Explicit types
    
    * Woo
    
    * oops
    
    ---------
    
    Co-authored-by: Zin <62830952+Zintixx@users.noreply.github.com>

--- Maple2.Model/Metadata/Constants.cs
+++ Maple2.Model/Metadata/Constants.cs
@@ -13,8 +13,8 @@ public static class Constant {
 
     public const int ServerMaxCharacters = 8;
     public const int CharacterNameLengthMax = 12;
-    public const long MaxMeret = 99999;
-    public const long MaxMeso = 999999999;
+    public const long MaxMeret = long.MaxValue;
+    public const long MaxMeso = long.MaxValue;
     public const long StarPointMax = 999999;
     public const long MesoTokenMax = 100000;
     public const int MaxSkillTabCount = 3;
