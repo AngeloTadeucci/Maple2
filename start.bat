3d411cab0b3f45c9968532398a728da1fc7dd68d
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2025-04-01 01:25:36 -0300

    Update batch files to use --no-build flag

--- start.bat
+++ start.bat
@@ -2,7 +2,7 @@
 
 dotnet build
 
-wt -d "Maple2.Server.World" --title "World Server" dotnet run ; ^
-nt -d "Maple2.Server.Login" --title "Login Server" dotnet run ; ^
-nt -d "Maple2.Server.Web" --title "Web Server" dotnet run ; ^
-nt -d "Maple2.Server.Game" --title "Game Server" dotnet run
+wt -d "Maple2.Server.World" --title "World Server" dotnet run --no-build ; ^
+nt -d "Maple2.Server.Login" --title "Login Server" dotnet run --no-build ; ^
+nt -d "Maple2.Server.Web" --title "Web Server" dotnet run --no-build ; ^
+nt -d "Maple2.Server.Game" --title "Game Server" dotnet run --no-build
