43f554412de2632f4f3fd0fb4463c69d35b1426a
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2025-04-01 20:40:55 -0300

    Fix Trigger UserValue Comparator
    Also automatically replace trigger scripts in the game server folder ...

--- Maple2.Server.World/Migrations/20250401090105_PlayerReport.cs
+++ Maple2.Server.World/Migrations/20250401090105_PlayerReport.cs
@@ -4,18 +4,14 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Maple2.Server.World.Migrations
-{
+namespace Maple2.Server.World.Migrations {
     /// <inheritdoc />
-    public partial class PlayerReport : Migration
-    {
+    public partial class PlayerReport : Migration {
         /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
+        protected override void Up(MigrationBuilder migrationBuilder) {
             migrationBuilder.CreateTable(
                 name: "player-reports",
-                columns: table => new
-                {
+                columns: table => new {
                     Id = table.Column<long>(type: "bigint", nullable: false)
                         .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
                     CharacterId = table.Column<long>(type: "bigint", nullable: false),
@@ -31,16 +27,14 @@ namespace Maple2.Server.World.Migrations
                         .Annotation("MySql:CharSet", "utf8mb4"),
                     CreateTime = table.Column<DateTime>(type: "datetime(6)", nullable: false)
                 },
-                constraints: table =>
-                {
+                constraints: table => {
                     table.PrimaryKey("PK_player-reports", x => x.Id);
                 })
                 .Annotation("MySql:CharSet", "utf8mb4");
         }
 
         /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
+        protected override void Down(MigrationBuilder migrationBuilder) {
             migrationBuilder.DropTable(
                 name: "player-reports");
         }
