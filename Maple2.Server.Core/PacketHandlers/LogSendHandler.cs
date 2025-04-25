0deb2aadc6d4edb47af5f6aadfa9889f37bb74af
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2025-04-25 01:35:49 -0300

    fix(logsendhandler): improve last packet logging with hex string output

--- Maple2.Server.Core/PacketHandlers/LogSendHandler.cs
+++ Maple2.Server.Core/PacketHandlers/LogSendHandler.cs
@@ -35,7 +35,7 @@ public abstract class LogSendHandler<T> : PacketHandler<T> where T : Session {
                         byte[]? lastPacket = session.GetLastSentPacket(exceptionInfo.SendOp);
 
                         Logger.Error("[{message}] [SendOp: {sendOp}] [Offset: {offset}] [Hint: {hint}] [LastPacket:\n{packetDump}]",
-                            message, exceptionInfo.SendOp, exceptionInfo.Offset, exceptionInfo.Hint, lastPacket);
+                            message, exceptionInfo.SendOp, exceptionInfo.Offset, exceptionInfo.Hint, lastPacket?.ToHexString(lastPacket.Length, ' ') ?? "No packet found");
                     } catch (ArgumentException) {
                         Logger.Error("[{Message}] {Debug}", message, debug);
                     }
