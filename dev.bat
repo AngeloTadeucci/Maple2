7bbedaadadf9d426f89b30e659ea354d984fad2b
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2025-05-12 11:06:10 +0200

    Fall back to cmd if windows terminal isn't installed

--- dev.bat
+++ dev.bat
@@ -2,6 +2,13 @@
 
 dotnet build
 
-wt -d "Maple2.Server.World" --title "World Server" dotnet run --no-build ; ^
-sp -d "Maple2.Server.Login" --title "Login Server" dotnet run --no-build ; ^
-sp -d "Maple2.Server.Web" --title "Web Server" dotnet run --no-build
+where wt >nul 2>&1
+if %errorlevel%==0 (
+    wt -d "Maple2.Server.World" --title "World Server" dotnet run --no-build ; ^
+    sp -d "Maple2.Server.Login" --title "Login Server" dotnet run --no-build ; ^
+    sp -d "Maple2.Server.Web" --title "Web Server" dotnet run --no-build
+) else (
+    start "World Server" /d "Maple2.Server.World" cmd /k dotnet run --no-build
+    start "Login Server" /d "Maple2.Server.Login" cmd /k dotnet run --no-build
+    start "Web Server" /d "Maple2.Server.Web" cmd /k dotnet run --no-build
+)
--- start.bat
+++ start.bat
@@ -2,7 +2,15 @@
 
 dotnet build
 
-wt -d "Maple2.Server.World" --title "World Server" dotnet run --no-build ; ^
-nt -d "Maple2.Server.Login" --title "Login Server" dotnet run --no-build ; ^
-nt -d "Maple2.Server.Web" --title "Web Server" dotnet run --no-build ; ^
-nt -d "Maple2.Server.Game" --title "Game Server" dotnet run --no-build
+where wt >nul 2>&1
+if %errorlevel%==0 (
+    wt -d "Maple2.Server.World" --title "World Server" dotnet run --no-build ; ^
+    nt -d "Maple2.Server.Login" --title "Login Server" dotnet run --no-build ; ^
+    nt -d "Maple2.Server.Web" --title "Web Server" dotnet run --no-build ; ^
+    nt -d "Maple2.Server.Game" --title "Game Server" dotnet run --no-build
+) else (
+    start "World Server" /d "Maple2.Server.World" cmd /k dotnet run --no-build
+    start "Login Server" /d "Maple2.Server.Login" cmd /k dotnet run --no-build
+    start "Web Server" /d "Maple2.Server.Web" cmd /k dotnet run --no-build
+    start "Game Server" /d "Maple2.Server.Game" cmd /k dotnet run --no-build
+)
