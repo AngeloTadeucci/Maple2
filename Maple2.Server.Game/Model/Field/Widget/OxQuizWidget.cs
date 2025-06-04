caa3f69ce0754edc7fb18aba0c0bb2bf0deb9876
Zin <62830952+Zintixx@users.noreply.github.com>
2025-06-03 20:00:57 -0700

    remove reflection

--- Maple2.Server.Game/Model/Field/Widget/OxQuizWidget.cs
+++ Maple2.Server.Game/Model/Field/Widget/OxQuizWidget.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using Maple2.Model.Metadata;
 using Maple2.Server.Game.Manager.Field;
 using Maple2.Server.Game.Packets;
+using Serilog;
 
 namespace Maple2.Server.Game.Model.Widget;
 
@@ -22,9 +23,31 @@ public class OxQuizWidget : Widget {
     }
 
     public override void Action(string function, int numericArg, string stringArg) {
-        MethodInfo? method = GetType().GetMethod(function, BindingFlags.NonPublic | BindingFlags.Instance);
-        if (method != null) {
-            method.Invoke(this, [stringArg, numericArg]);
+        switch (function) {
+            case "DevMode":
+                DevMode(stringArg, numericArg);
+                break;
+            case "PickQuiz":
+                PickQuiz(stringArg, numericArg);
+                break;
+            case "ShowQuiz":
+                ShowQuiz(stringArg, numericArg);
+                break;
+            case "ShowAnswer":
+                ShowAnswer(stringArg, numericArg);
+                break;
+            case "PreJudge":
+                PreJudge(stringArg, numericArg);
+                break;
+            case "Judge":
+                Judge(stringArg, numericArg);
+                break;
+            case "Winner":
+                Winner(stringArg, numericArg);
+                break;
+            default:
+                Log.Logger.Warning("Unknown function called on OxQuizWidget: {Function}", function);
+                break;
         }
     }
 
