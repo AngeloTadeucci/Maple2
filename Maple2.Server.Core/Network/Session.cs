495b04424c1f928fbe79cc64724f4872fdef6c94
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2025-06-27 22:03:07 -0300

    Fix: dungeon migrating (#519)

--- Maple2.Server.Core/Network/Session.cs
+++ Maple2.Server.Core/Network/Session.cs
@@ -211,6 +211,11 @@ public abstract class Session : IDisposable {
                 reader.AdvanceTo(buffer.Start, buffer.End);
             } while (!disposed && !result.IsCompleted);
         } catch (Exception ex) {
+            if (ex is InvalidOperationException invalidOperation && invalidOperation.Message.Contains("reader was completed")) {
+                // Ignore this exception, it happens when the reader is completed, either by the client closing or by the session being disposed.
+                // it's not a real error
+                return;
+            }
             if (!disposed) {
                 Logger.Error(ex, "Exception reading recv packet");
             }
