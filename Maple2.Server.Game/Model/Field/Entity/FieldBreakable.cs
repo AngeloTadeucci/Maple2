da2715d14d7817d1b521b24b11e4ff89653261d3
Ângelo Tadeucci <15664821+AngeloTadeucci@users.noreply.github.com>
2025-08-03 00:23:19 -0300

    feat: add SuperArmorBreak to damage evade logic and fix breakable visibility handling (#555)

--- Maple2.Server.Game/Model/Field/Entity/FieldBreakable.cs
+++ Maple2.Server.Game/Model/Field/Entity/FieldBreakable.cs
@@ -56,6 +56,10 @@ public class FieldBreakable : FieldEntity<BreakableActor> {
             return;
         }
 
+        if (!Visible) {
+            return;
+        }
+
         switch (State) {
             case BreakableState.Show:
                 break;
--- Maple2.Server.Game/Model/Skill/DamagePropertyRecord.cs
+++ Maple2.Server.Game/Model/Skill/DamagePropertyRecord.cs
@@ -3,24 +3,14 @@
 namespace Maple2.Server.Game.Model.Skill;
 
 public class DamagePropertyRecord {
-    public bool CanCrit { get; init; }
-    public Element Element { get; init; }
-    public int SkillId { get; init; }
-    public int SkillGroup { get; init; }
-    public RangeType RangeType { get; init; }
+    public bool CanCrit { get; init; } = true;
+    public Element Element { get; init; } = Element.None;
+    public int SkillId { get; init; } = 0;
+    public int SkillGroup { get; init; } = 0;
+    public RangeType RangeType { get; init; } = RangeType.None;
     public AttackType AttackType { get; init; }
-    public CompulsionType[] CompulsionTypes { get; init; }
-    public float Rate { get; init; }
-    public long Value { get; init; }
-
-    public DamagePropertyRecord() {
-        CanCrit = true;
-        Element = Element.None;
-        SkillId = 0;
-        SkillGroup = 0;
-        RangeType = RangeType.None;
-        CompulsionTypes = [];
-        Rate = 0;
-        Value = 0;
-    }
+    public CompulsionType[] CompulsionTypes { get; init; } = [];
+    public float Rate { get; init; } = 0;
+    public long Value { get; init; } = 0;
+    public bool SuperArmorBreak { get; set; }
 }
