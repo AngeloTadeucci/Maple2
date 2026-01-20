b9e0bf1906b24e9e75308be9f9223c362da09c44
Zin <62830952+Zintixx@users.noreply.github.com>
2026-01-19 21:40:04 -0800

    oops forgot this file

--- Maple2.Server.Game/Model/Field/Entity/FieldPlayerSpawnPoint.cs
+++ Maple2.Server.Game/Model/Field/Entity/FieldPlayerSpawnPoint.cs
@@ -5,10 +5,12 @@ namespace Maple2.Server.Game.Model;
 
 public class FieldPlayerSpawnPoint : FieldEntity<SpawnPointPC> {
     public bool Enable;
+    public int Id { get; init; }
 
-    public FieldPlayerSpawnPoint(FieldManager field, int objectId, SpawnPointPC metadata) : base(field, objectId, metadata) {
+    public FieldPlayerSpawnPoint(FieldManager field, int objectId, SpawnPointPC metadata, int id) : base(field, objectId, metadata) {
         Enable = metadata.Enable;
         Position = metadata.Position;
         Rotation = metadata.Rotation;
+        Id = id;
     }
 }
